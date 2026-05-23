45d43af4aae28880d5825e9b4f058515177f55a9
slayeryx <slayermaster211@gmail.com>
2026-05-23 00:06:09 -0400

    Create Get Key API.lua

---
+++ Library/Key System/Library/Key System/Library/Key System/Get Key API.lua
@@ -0,0 +1,18 @@
+return {
+  [6447798030] = { -- Funky-Friday
+    ["Linkvertise"] = {
+      Url = "",
+      Icon = 122637139040192,
+    },
+
+    ["LootLabs"] = {
+      Url = "",
+      Icon = 81598390322167,
+    },
+
+    ["Work.ink"] = {
+      Url = "",
+      Icon = 137084367454843,
+    },
+  },
+}
